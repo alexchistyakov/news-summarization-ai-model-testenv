3a1dd3e52af511ce19b7afced659c1bba74883ed
Alex Chistyakov <alex@DESKTOP-KVIO2P9>
2024-01-29 14:27:02 -0700

    Added testing for falconsai-textsummarization model

---
+++ falconsai-text-summarization/main.py
@@ -0,0 +1,33 @@
+from transformers import pipeline
+import json
+import time
+import nvidia_smi
+
+
+summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+file = open("../samples.json")
+samples = json.load(file)
+
+for article in samples["samples"]:
+
+    start = time.process_time()
+
+    #Run Model on sample
+    print(summarizer(article, max_length=130, min_length=30, do_sample=False))
+
+    #Print time elapsed
+    end = time.process_time()
+    print("---------------------")
+    print("Time elapsed: {0} ms".format(end - start))
+
+    #Show memory usage
+    nvidia_smi.nvmlInit()
+    deviceCount = nvidia_smi.nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+        info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+        print("Device {}: {}, Memory Used : ({:.2f}%) : {} bytes (total), {} bytes (free), {} bytes (used)".format(i, nvidia_smi.nvmlDeviceGetName(handle), 100-(100*info.free/info.total), info.total, info.free, info.used))
+
+    nvidia_smi.nvmlShutdown()
+    print("---------------------")
+
